a326ac025575bea1857f843ad58687feda958184
Brunao <prupomaia@hotmail.com>
2022-09-27 19:09:50 -0300

    completando a conexao com o banco de dados

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ Route::get('/', [EventController::class, 'index']);
 
 Route::get('/events/create', [EventController::class, 'create']);
 
+Route::post('/events',[EventController::class, 'store']);
+
 /*Route::get('/produtos/{id}',function($id){
     return view('product',['id' => $id ]);
 });*/
